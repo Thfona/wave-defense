8cc18710b7dd33461dc8d8a3b16f0216e5f38b0a
Thfona <thiagofn.1997@gmail.com>
2025-01-19 20:20:57 -0300

    add assets

--- Game1.cs
+++ Game1.cs
@@ -9,6 +9,19 @@ public class Game1 : Game
     private GraphicsDeviceManager graphics;
     private SpriteBatch spriteBatch;
 
+    private static readonly int screenWidth = 1280;
+    private static readonly int screenHeight = 720;
+
+    private Texture2D playerSprite;
+    private Texture2D walkDownSprite;
+    private Texture2D walkLeftSprite;
+    private Texture2D walkRightSprite;
+    private Texture2D walkUpSprite;
+
+    private Texture2D backgroundSprite;
+    private Texture2D ballSprite;
+    private Texture2D skullSprite;
+
     public Game1()
     {
         graphics = new GraphicsDeviceManager(this);
@@ -18,7 +31,9 @@ public class Game1 : Game
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
+        graphics.PreferredBackBufferWidth = screenWidth;
+        graphics.PreferredBackBufferHeight = screenHeight;
+        graphics.ApplyChanges();
 
         base.Initialize();
     }
@@ -27,7 +42,15 @@ public class Game1 : Game
     {
         spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        // TODO: use this.Content to load your game content here
+        playerSprite = Content.Load<Texture2D>("player/player");
+        walkDownSprite = Content.Load<Texture2D>("player/walkDown");
+        walkLeftSprite = Content.Load<Texture2D>("player/walkLeft");
+        walkRightSprite = Content.Load<Texture2D>("player/walkRight");
+        walkUpSprite = Content.Load<Texture2D>("player/walkUp");
+
+        backgroundSprite = Content.Load<Texture2D>("background");
+        ballSprite = Content.Load<Texture2D>("ball");
+        skullSprite = Content.Load<Texture2D>("skull");
     }
 
     protected override void Update(GameTime gameTime)
@@ -46,7 +69,11 @@ public class Game1 : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+        spriteBatch.Begin();
+
+        spriteBatch.Draw(backgroundSprite, new Vector2(-500, -500), Color.White);
+
+        spriteBatch.End();
 
         base.Draw(gameTime);
     }
